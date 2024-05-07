c3668c12a1154c54fecedf9e1ac6de2c57cba1a2
Martin Koloseus <mk_alternativ@posteo.de>
2024-05-07 15:40:06 +0200

    main: add main.cpp for testing (temporary)

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "graph.hpp"
 #include "two_respecting_trees.hpp"
 #include "two_respecting_cuts.hpp"
+#include "stoer_wagner.hpp"
+#include "random.hpp"
 
 WeightedGraph readWeightedGraph() {
     size_t numVertices;
@@ -25,20 +27,42 @@ WeightedGraph readWeightedGraph() {
     return WeightedGraph(numVertices, std::move(edges));
 }
 
+
+
 int main() {
+    /*
     WeightedGraph graph = readWeightedGraph();
+    std::cout << "Read" << std::endl;
+    */
+
+    WeightedGraph graph = randomConnectedWeightedGraph(10, 30, 1.0, 5.0);
+
+    
     TreePacking packing = findTwoRespectingTrees(graph, 3);
+    std::cout << "Packing, size = " << packing.trees().size() << std::endl;
     std::vector<VertexID> minCut;
     EdgeWeight minCutWeight = INFINITE_WEIGHT;
+
+
+
     for (const std::vector<size_t>& edgeSelection : packing.trees()) {
         RootedSpanningTree rst(graph, edgeSelection, 0);
+        //std::cout << "RST" << std::endl;
         auto smallestCut = findSmallest2RespectingCut(rst);
+        //std::cout << "smallest cut" << std::endl;
         if (smallestCut.second < minCutWeight) {
             minCutWeight = smallestCut.second;
             minCut = std::move(smallestCut.first);
         }
     }
-    std::cout << minCutWeight << std::endl;
+    
+    /*
+    auto minCutInfo = stoerWagner(graph);
+    const auto& minCut = minCutInfo.first;
+    auto minCutWeight = minCutInfo.second;*/
+
+    std::cout << "Min cut weight: " << minCutWeight << std::endl;
+    std::cout << "Vertices " << std::endl;
     for (VertexID vertex : minCut) {
         std::cout << vertex << '\n';
     }
